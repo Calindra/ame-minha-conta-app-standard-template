34d1fc8179b0eb9716958b6ee9d1bc29a287cabb
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-04-12 15:47:44 -0300

    refactor: version components and super-app

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,8 +5,8 @@
     </View>    
 		<View direction='column' align='center' justify='center'>
       <Text textAlign="center" fontSize="xxxs" color="neutralcolor-darkest">Este mini-app foi desenvolvido utilizando as seguintes versões:</Text>
-        <Text textAlign="center">Versão da biblioteca de componentes: 2.5.0</Text>
-  			<Text textAlign="center">Versão da API de Super Client: 2.8.0</Text>
+        <Text textAlign="center">Versão da biblioteca de componentes: 0.6.0</Text>
+  			<Text textAlign="center">Versão da API de Super Client: 1.0.1</Text>
 		</View>
 		<Button
       text="Home"
