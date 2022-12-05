947aeb431d1de7daccf390148c5233e68f2a3ee4
João Victor Bartolot <joao.bartolot@calindra.com.br>
2022-12-05 12:40:55 -0300

    feat: adicao do sdk-support

--- ame.conf.js
+++ ame.conf.js
@@ -11,5 +11,6 @@ module.exports = {
     "public-key": "c292c513-e563-4fe7-8e9d-cbe95c33e498",
     "ame-super-app-client-web": "1.0.1",
     "ame-miniapp-components-web": "0.6.0",
+    "ame-mini-app-sdk-support": "1.0.0",
     "version": "0.1.0",
 }
